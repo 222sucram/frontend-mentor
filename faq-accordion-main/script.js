dc7b5f6df4340d8e202ef22d9cae70f57c8dbfc7
Marcus <marcus.oflaherty222@gmail.com>
2024-07-31 00:52:48 +0100

    add tabindex navigation

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -11,4 +11,20 @@ buttons.forEach(btn => {
         e.target.classList.toggle('active')
         e.target.nextElementSibling.classList.toggle('hide')
     })
+    
+    btn.addEventListener('keyup', (e) => {
+        if(e.code == 'Enter') {
+            console.log(e.target)
+            buttons.forEach(btn => {
+                btn.classList.remove('active')
+                btn.nextElementSibling.classList.add('hide')
+            })
+            e.target.classList.toggle('active')
+            btn.nextElementSibling.classList.toggle('hide')
+            
+
+            // try set status of target
+            // then loop thru all btns and deactivate
+        }
+    })
 });
